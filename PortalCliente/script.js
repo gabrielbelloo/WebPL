428d4ff3cb595688de94a4b698fca1afad4858da
Gabriel Bello <bellodealmeidagabriel@gmail.com>
2025-04-17 15:53:45 -0300

    Create editProfile()

--- PortalCliente/script.js
+++ PortalCliente/script.js
@@ -1,28 +1,25 @@
-function mudaMargemBanner(){
+function mudaMargemBanner() {
     var alturaHeader = document.getElementById('header').offsetHeight;
     document.getElementById('banner').style.marginTop = alturaHeader + 'px';
 }
 
-function mudaComprimentoOptions(){
+function mudaComprimentoOptions() {
     var larguraSelect = document.getElementById('empresas-box').offsetWidth;
     var options = document.getElementById('options');
     options.style.width = larguraSelect + 'px';
 }
 
-function editProfile(){
-    var editProfile = document.getElementById('edit-profile');
-    var profile = document.getElementById('profile');
-    if(editProfile.style.display === 'block'){
-        editProfile.style.display = 'none';
-        profile.style.display = 'block';
-    }else{
-        editProfile.style.display = 'block';
-        profile.style.display = 'none';
+function editProfile() {
+    var editModal = document.getElementById('editModal');
+    if (editModal.style.display === 'block') {
+        editModal.style.display = 'none';
+    } else {
+        editModal.style.display = 'block';
     }
 }
 
-function logout(){
-    
+function logout() {
+
 }
 
 function removeEmpresa(element) {
@@ -37,17 +34,17 @@ function limparEmpresa(element) {
     empresasContainer.innerHTML = '';
 }
 
-function openEmpresasOptions(){
+function openEmpresasOptions() {
     var options = document.getElementById('options');
-        if(options.style.display === 'block'){
-            options.style.display = 'none';
-        }else{
+    if (options.style.display === 'block') {
+        options.style.display = 'none';
+    } else {
         options.style.display = 'block';
-        }
-        document.getElementById("fa-chevron-down").classList.toggle("girar");
+    }
+    document.getElementById("fa-chevron-down").classList.toggle("girar");
 }
 
-window.onload = function() {
+window.onload = function () {
     mudaMargemBanner();
     mudaComprimentoOptions();
 };
