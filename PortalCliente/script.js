13460be99f10fd11c8dc346fa1b6719c33a12be9
Gabriel Bello <bellodealmeidagabriel@gmail.com>
2025-04-22 15:38:53 -0300

    Create closeEditModal()

--- PortalCliente/script.js
+++ PortalCliente/script.js
@@ -18,6 +18,11 @@ function editProfile() {
     }
 }
 
+function closeEditModal() {
+    var editModal = document.getElementById('editModal');
+    editModal.style.display = 'none';
+}
+
 function logout() {
 
 }
